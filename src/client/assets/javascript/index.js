99b20b07e46899cd9a42f2fa3c497497521e35fc
Laurens Deprost <ldeprost@gmail.com>
2020-08-30 16:03:59 +0200

    chore: Test server change

--- src/client/assets/javascript/index.js
+++ src/client/assets/javascript/index.js
@@ -395,7 +395,8 @@ function renderAt(element, html) {
 
 // API CALLS ------------------------------------------------
 
-const SERVER = "http://localhost:8000";
+// const SERVER = "http://localhost:8000";
+const SERVER = "";
 
 function defaultFetchOpts() {
   return {
--- src/server/index.js
+++ src/server/index.js
@@ -5,6 +5,7 @@ const path = require("path");
 
 const app = express();
 const DEFAULT_PORT = 3000;
+const SERVER = "http://localhost:8000";
 
 // setup the ability to see into response bodies
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -24,6 +25,39 @@ app.get("/", async (req, res) => {
 
 app.get("/race", async (req, res) => {
   res.sendFile(path.join(__dirname, "../client/pages/race.html"));
+});
+
+const addTrackNames = (tracks) =>
+  tracks.map((track) => {
+    switch (track.name) {
+      case "Track 1":
+        track.name = "Vengeance";
+        break;
+      case "Track 2":
+        track.name = "Sunken City";
+        break;
+      case "Track 3":
+        track.name = "Inferno";
+        break;
+      case "Track 4":
+        track.name = "Executioner";
+        break;
+      case "Track 5":
+        track.name = "Abyss";
+        break;
+      case "Track 6":
+        track.name = "The Gauntlet";
+        break;
+    }
+    return track;
+  });
+
+app.get("/api/tracks", async (req, res) => {
+  res.send(
+    fetch(`${SERVER}/api/tracks`)
+      .then((res) => res.json())
+      .then((data) => addTrackNames(data))
+  );
 });
 
 const currentPort = process.env.PORT || DEFAULT_PORT;
